7939b4b8d075c7680052fb706381a7ae6f402df6
Arjun NM <arjun@curriculumindia.com>
2025-10-23 22:23:45 +0530

    change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const port = 6500;
 const express = require("express");
 
 const app = express();
-app.get("/",(req,res)=>{
+app.get("/asset",(req,res)=>{
     //console.log("api fetch")
     return res.send("its ok yaar");
 
